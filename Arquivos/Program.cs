e8205e72813c8e26e7597ae3237ad287b8466813
SaCosta_pckt <sabrina.costa06@outlook.com>
2022-01-22 00:30:26 -0300

    criando e escrevendo em arquivos

---
+++ Arquivos/Program.cs
@@ -0,0 +1,8 @@
+﻿using static System.Console;
+
+var path = Path.Combine(Environment.CurrentDirectory,"teste.txt");
+
+var sw = File.CreateText(path);
+sw.WriteLine("Linha 1 teste teste");
+sw.WriteLine("Linha 2 teste teste");
+sw.Flush();
