c316a6a0505599623863cb056229b57e6f6acc77
SaCosta_pckt <sabrina.costa06@outlook.com>
2022-01-23 17:04:15 -0300

    Tratando erros e criando projeto de criação de diretórios

--- Arquivos/Program.cs
+++ Arquivos/Program.cs
@@ -1,8 +1,35 @@
 ﻿using static System.Console;
 
-var path = Path.Combine(Environment.CurrentDirectory,"teste.txt");
+WriteLine("Escreva o nome do arquivo:");
+var nome = LimparNome(ReadLine());
 
-var sw = File.CreateText(path);
-sw.WriteLine("Linha 1 teste teste");
-sw.WriteLine("Linha 2 teste teste");
-sw.Flush();
+var path = Path.Combine(Environment.CurrentDirectory,$"{nome}.txt");
+
+
+CriarArquivo(path);
+WriteLine("Digite enter para finalizar...");
+ReadLine();
+
+static void CriarArquivo(string path){
+    //Escrever "using" faz com que o programa dê o "flush"(descargue as linhas escritas na memória pro arquivo) automaticamente
+
+    try{
+        using var sw = File.CreateText(path);
+        sw.WriteLine("Linha 1 teste teste");
+        sw.WriteLine("Linha 2 teste teste");
+    }
+
+    catch{
+        WriteLine("Nome de arquivo inválido!");
+    }
+}
+
+static string LimparNome(string nome)
+{
+    //Evitar criação de arquivo com nome inválido
+    foreach (var caractere in Path.GetInvalidFileNameChars())
+    {
+        nome = nome.Replace(caractere,'-');
+    }
+    return nome;
+}
---
+++ Diretórios/Program.cs
@@ -0,0 +1,45 @@
+﻿// See https://aka.ms/new-console-template for more information
+using static System.Console;
+
+CriarDiretoriosGlobo();
+
+static void CriarDiretoriosGlobo()
+{
+    var path = Path.Combine(Environment.CurrentDirectory, "globo");
+
+    if (!Directory.Exists(path))
+    {
+        var dirGlobo = Directory.CreateDirectory(path);
+
+        var dirAmerica = dirGlobo.CreateSubdirectory("Américas");
+        var dirAsia = dirGlobo.CreateSubdirectory("Ásia");
+        var dirAfrica = dirGlobo.CreateSubdirectory("África");
+        var dirEuropa = dirGlobo.CreateSubdirectory("Europa");
+        var dirOceania = dirGlobo.CreateSubdirectory("Oceania");
+
+        dirAmerica.CreateSubdirectory("Brasil");
+        dirAmerica.CreateSubdirectory("Canada");
+        dirAmerica.CreateSubdirectory("Argentina");
+
+        dirAsia.CreateSubdirectory("Japao");
+        dirAsia.CreateSubdirectory("China");
+
+        dirAfrica.CreateSubdirectory("Nigeria");
+        dirAfrica.CreateSubdirectory("Marrocos");
+
+        dirEuropa.CreateSubdirectory("França");
+        dirEuropa.CreateSubdirectory("Portugal");
+
+        dirOceania.CreateSubdirectory("Palau");
+        dirOceania.CreateSubdirectory("Australia");
+
+        WriteLine("Diretórios criados com sucesso!");
+    }
+
+    else
+    {
+        WriteLine("Os diretórios já existem!");
+    }
+}
+
+
